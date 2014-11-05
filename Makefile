6d83bdc155b6aa12e0b807b5922127127f9f94d6
Alexey Melnikov <individum05@gmail.com>
2014-11-05 17:14:50 +0600

    Programm prints training test data. Fix Makefile

--- Makefile
+++ Makefile
@@ -5,16 +5,16 @@ CFLAGS = -g
 
 all: test xor digits
 
-test: nnetwork.o
+test: nnetwork.o test.cpp
 	$(CC) $(CFLAGS) test.cpp nnetwork.o -o test
 
-xor: nnetwork.o
+xor: nnetwork.o xor.cpp
 	$(CC) $(CFLAGS) xor.cpp nnetwork.o -o xor
 
-digits: nnetwork.o
+digits: nnetwork.o digits.cpp
 	$(CC) $(CFLAGS) digits.cpp nnetwork.o -o digits
 
-nnetwork.o:
+nnetwork.o: nnetwork.cpp
 	$(CC) $(CFLAGS) -c nnetwork.cpp
 
 clean:
--- digits.cpp
+++ digits.cpp
@@ -1,49 +1,10 @@
 #include "nnetwork.h"
 #include <fstream>
+#include <string>
 
 using namespace std;
 
 const size_t train_cnt = 13;
-
-double dig1[25] = {0, 0, 0, 1, 0,
-				   0, 0, 0, 1, 0,
-				   0, 0, 0, 1, 0,
-				   0, 0, 0, 1, 0,
-				   0, 0, 1, 1, 1
-				};
-vector <double> v_dig1(dig1, dig1 + 25);
-double rdig1[3] = {0.1, 0.9, 0.1};
-vector <double> r_dig1(rdig1, rdig1 + 3);
-
-double nad1[25] = {0, 0, 0, 0, 0,
-				   0, 0, 0, 0, 0,
-				   0, 0, 0, 0, 0,
-				   0, 0, 0, 0, 0,
-				   0, 0, 0, 0, 0
-				};
-vector <double> v_nad1(nad1, nad1 + 25);
-double rnad1[3] = {0.1, 0.1, 0.1};
-vector <double> r_nad1(rnad1, rnad1 + 3);
-
-double dig2[25] = {1, 1, 1, 1, 1,
-				   0, 0, 0, 0, 1,
-				   1, 1, 1, 1, 1,
-				   1, 0, 0, 0, 0,
-				   1, 1, 1, 1, 1
-				};
-vector <double> v_dig2(dig2, dig2 + 25);
-double rdig2[3] = {0.1, 0.1, 0.9};
-vector <double> r_dig2(rdig2, rdig2 + 3);
-
-double dig0[25] = {0, 1, 1, 1, 1,
-				   0, 1, 0, 0, 1,
-				   0, 1, 0, 0, 1,
-				   0, 1, 0, 0, 1,
-				   0, 1, 1, 1, 1
-				};
-vector <double> v_dig0(dig0, dig0 + 25);
-double rdig0[3] = {0.9, 0.1, 0.1};
-vector <double> r_dig0(rdig0, rdig0 + 3);
 
 double deformed0[25] = {0, 1, 1, 1, 1,
 					    0, 0, 0, 0, 1,
@@ -94,6 +55,25 @@ vector <double> train_digit(NeuralNetwork &dignet, vector <double> &vec,
 	return dignet.train(vec, testvec, alpha, nu);
 }
 
+void print_mostsimilar(vector <double> &vec)
+{
+	double max = vec[0];
+	int maxi = 0;
+	string ret;
+
+	for (int i = 1; i < vec.size(); i++) {
+		if (max < vec[i]) {
+			max = vec[i];
+			maxi = i;
+		}
+	}
+	if (max > 0.5)
+		cout << maxi << " Out is " << max << endl;
+	else
+		cout << "Not a digit! Most similar digit is " << maxi << " Out is " <<
+		 max << endl;
+}
+
 int main()
 {
 	const unsigned int dig_layers[3] = {25, 25, 10};
@@ -122,42 +102,21 @@ int main()
 		
 		train_digit(dignet, digit_vec[num], digit_n[num], 0.1, 0.3);
 	}
-	
+	cout << "Training data:" << endl;
+	for (int i = 0; i < train_cnt; i++) {
+		res = dignet.evaluate(digit_vec[i]);
+		if (digit_n[i] >= 0)
+			cout << digit_n[i] << endl;
+		else
+			cout << "Not a digit" << endl;
+		vector_print(res);
+		print_mostsimilar(res);
+		cout << endl;
+	}
+
 	res = dignet.evaluate(digit_vec[0]);
 	cout << endl << "0" << endl;
 	vector_print(res);
-	
-/*
-	for (int i = 0; i < 1000; i++) {
-		dignet.train(v_dig1, r_dig1, 0.1, 0.3);
-		dignet.train(v_dig2, r_dig2, 0.1, 0.3);
-		dignet.train(v_nad1, r_nad1, 0.1, 0.3);
-		dignet.train(v_dig0, r_dig0, 0.1, 0.3);
-	}
 
-	res = dignet.evaluate(v_dig0);
-	cout << endl << "0" << endl;
-	vector_print(res);
-
-	res = dignet.evaluate(v_dig1);
-	cout << endl << "1" << endl;
-	vector_print(res);
-
-	res = dignet.evaluate(v_dig2);
-	cout << endl << "2" << endl;
-	vector_print(res);
-	
-	res = dignet.evaluate(v_deformed0);
-	cout << endl << "deformed 0" << endl;
-	vector_print(res);
-
-	res = dignet.evaluate(v_deformed1);
-	cout << endl << "deformed 1" << endl;
-	vector_print(res);
-	
-	res = dignet.evaluate(v_nad1);
-	cout << endl << "Not a digit" << endl;
-	vector_print(res);
-*/
 	return 0;
 }
